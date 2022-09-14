c53090fc85e1df35a09f394fc4117ebef89be8e2
asorokin2107 <anatolysorokin2107@gmail.com>
2022-09-14 14:35:54 +0300

    new variable

--- Lesson 2.0 Calculator gb to mb/src/Test.java
+++ Lesson 2.0 Calculator gb to mb/src/Test.java
@@ -4,8 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Введите гб:");
         long gb = new Scanner(System.in).nextLong();
-        long mb = 1024 * gb;
-        long b = 1024 * mb;
+        int meaning = 1024;
+        long mb = meaning * gb;
+        long b = meaning * mb;
 
         System.out.println("B " + gb + " гигабайтах " + mb + " мегабайт, или " + b + " байт");
 
